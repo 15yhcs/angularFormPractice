a9d42c512ad9231ceff6cf8f6b428211b8d12dba
Yewin Shu <15yhcs@queensu.ca>
2024-05-08 15:34:25 -0400

    Complete form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
 import { NgForm, NgModel } from '@angular/forms';
+import { __values } from 'tslib';
 
 @Component({
   selector: 'app-root',
@@ -11,8 +12,32 @@ export class AppComponent {
   @ViewChild('email') e:NgModel;
   answer = '';
   defaultQuestion= "pet";
+  // suggestedName: string;
+  gender = ['male', 'female']
+  user = {
+    uName: '',
+    mail: '',
+    secret: '',
+    qAnswered: '',
+    gender: ''
+  }
   suggestUserName() {
+    // this.suggestedName = 'Superuser';
     const suggestedName = 'Superuser';
+    // this.form.setValue({
+    //   userData: {
+    //     username: suggestedName,
+    //     mail: "email@email.com"
+    //   },
+    //   secret: this.defaultQuestion,
+    //   questionAnswered: "",
+    //   genders: 'male'
+    // })
+    this.form.form.patchValue({
+      userData: {
+        username: suggestedName
+      }
+    })
   }
 
   // onSubmit(ff: NgForm){
@@ -21,9 +46,12 @@ export class AppComponent {
     
   // }
   onSubmit(){
-    console.log('Submitted');
-    console.log(this.form);
-    console.log(this.e);
-    
+    this.user.uName = this.form.value.userData.username;
+    this.user.mail = this.form.value.userData.mail;
+    this.user.qAnswered = this.form.value.questionAnswered;
+    this.user.secret = this.form.value.secret;
+    this.user.gender = this.form.value.genders;
+
+    this.form.reset();
   }
 }
